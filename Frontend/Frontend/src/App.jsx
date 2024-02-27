4666219db2b5fde1e711439c353cda14681e77a1
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2024-02-27 22:02:01 +0100

    Upogonjeni frontend

---
+++ Frontend/Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <>
+      <div>
+
+      <h1>MyAPP</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ Frontend/Frontend/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import Container from 'react-bootstrap/Container';
+
+export default function Pocetna() {
+
+    return (
+        <>
+        <Container>
+            This is my first frontend
+        </Container>
+        </>
+    );
+}
