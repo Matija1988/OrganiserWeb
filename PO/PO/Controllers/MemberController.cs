5bc7520b0e8002cb2ffb918adafad58274513c2c
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2024-03-29 20:14:56 +0100

    Implementacija apstraktnog controllera na members controlleru

--- PO/PO/Controllers/MemberController.cs
+++ PO/PO/Controllers/MemberController.cs
@@ -4,6 +4,7 @@ using PO.Data;
 using PO.Models;
 using PO.Extensions;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
 
 namespace PO.Controllers
 {
@@ -14,216 +15,34 @@ namespace PO.Controllers
     /// </summary>
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class MemberController : ControllerBase
+    public class MemberController : POController<Member, MemberDTORead, MemberDTOInsertUpdate>
     {
-
-        /// <summary>
-        /// Kontekst za rad s bazom koji ce biti postavljen pomocu ubrizgavanja ovisnosti
-        /// Context for DB 
-        /// </summary>
-
-        private readonly POContext _context;
-
-        /// <summary>
-        /// Konstruktor klase koja prima PO kontekst pomocu ubrizgavanja ovisnosti
-        /// Constructor that recieves PO context from dependency injection
-        /// </summary>
-        /// <param name="context"></param>
-
-        public MemberController(POContext context)
+        public MemberController(POContext context) : base(context)
         {
-            _context = context;
+            DbSet = _context.members;
         }
-        /// <summary>
-        /// Dohvaca sve members (clanove) iz baze
-        /// Fetches all members from DB
-        /// </summary>
-        /// <param name="member"></param>
-        /// <returns></returns>
-        /// <response> code="200">Sve ok, ako nema podataka content-lenght:0</response>
-        /// <response> code="400">Zahtjev nije valjan</response>
-        /// <response> code="503">Baza na koju se spajam nije dostupna</response>
 
-
-        [HttpGet]
-        public IActionResult Get()
+        protected override void ControlDelete(Member entity)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
+            var entityFromDB = _context.members.Include(x => x.IActivities).FirstOrDefault(x => x.ID == entity.ID);
+
+            if (entityFromDB == null) 
+            { 
+            throw new Exception("Member with "  +  entity.ID + " not found. Please check your input!");  
             }
 
-            try
+            if(entityFromDB != null && entityFromDB.IActivities.Count() > 0)
             {
-                var memberDB = _context.members.ToList();
-
-                if (memberDB == null || memberDB.Count == 0)
+                StringBuilder sb = new StringBuilder();
+                sb.Append("Cannot delete member: Member is placed in activities: ");
+                foreach(var e in  entityFromDB.IActivities)
                 {
-                    return new EmptyResult();
+                    sb.Append(e.activityName).Append(", ");
                 }
-
-                return new JsonResult(memberDB.MapMemberReadList());
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status503ServiceUnavailable, ex.Message);
+                throw new Exception(sb.ToString().Substring(0, sb.ToString().Length - 2));
             }
 
         }
-
-        /// <summary>
-        /// Dodaje novog clana u bazu
-        /// Adds new member into DB
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="member"></param>
-        /// <returns></returns>
-        /// <response> code="200">Sve ok, ako nema podataka content-lenght:0</response>
-        /// <response> code="400">Zahtjev nije valjan</response>
-        /// <response> code="503">Baza na koju se spajam nije dostupna</response>
-
-        /// <summary>
-        /// Dohvaca clana sa odgovarajucom vrijednosti ID
-        /// Fetches member with correspoding ID from the DB
-        /// </summary>
-        /// <returns></returns>
-        /// <response> code="200">Sve ok, ako nema podataka content-lenght:0</response>
-        /// <response> code="400">Zahtjev nije valjan</response>
-        /// <response> code="503">Baza na koju se spajam nije dostupna</response>
-
-        [HttpGet]
-        [Route("{id:int}")]
-
-        public IActionResult Get(int id)
-        {
-            if (!ModelState.IsValid) { return BadRequest(ModelState); }
-
-            try
-            {
-                var m = _context.members.Find(id);
-                if (m == null)
-                {
-                    return new EmptyResult();
-                }
-                return new JsonResult(m.MapMemberInsertUpdateToDTO());
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status503ServiceUnavailable,
-                    ex.Message);
-            }
-
-        }
-
-        [HttpPost]
-        public IActionResult Post(MemberDTOInsertUpdate memberDTO)
-        {
-
-            if (!ModelState.IsValid || memberDTO == null) { return BadRequest(); }
-
-            try
-            {
-                var member = memberDTO.MapSmjerInsertUpdateFromDTO(new Member());
-                _context.members.Add(member);
-                _context.SaveChanges();
-                return StatusCode(StatusCodes.Status200OK, member.MapMemberReadToDTO());
-            }
-
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status503ServiceUnavailable, ex.Message);
-
-            }
-
-        }
-
-        /// <summary>
-        /// Mijenja postojeceg clana u bazi
-        /// Updates existing member from DB
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="member"></param>
-        /// <returns></returns>
-        /// <response> code="200">Sve ok, ako nema podataka content-lenght:0</response>
-        /// <response> code="400">Zahtjev nije valjan</response>
-        /// <response> code="503">Baza na koju se spajam nije dostupna</response>
-
-
-
-
-
-        [HttpPut]
-        [Route("{id:int}")]
-
-        public IActionResult Put(int id, MemberDTOInsertUpdate member)
-        {
-            if (id == 0 || !ModelState.IsValid || member == null)
-            {
-                return BadRequest();
-            }
-
-            try
-            {
-
-                var memberFromDB = _context.members.Find(id);
-
-
-                if (memberFromDB == null) { return StatusCode(StatusCodes.Status204NoContent, id); }
-
-                var entity = member.MapSmjerInsertUpdateFromDTO(memberFromDB);
-
-                _context.members.Update(memberFromDB);
-                _context.SaveChanges();
-
-                return StatusCode(StatusCodes.Status200OK, memberFromDB.MapMemberInsertUpdateToDTO());
-
-            } catch (Exception ex) {
-
-                return StatusCode(StatusCodes.Status503ServiceUnavailable, ex.Message);
-            }
-        }
-
-        /// <summary>
-        /// Brise clana iz baze
-        /// Deletes existing member from DB
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        /// <response> code="200">Sve ok, ako nema podataka content-lenght:0</response>
-        /// <response> code="400">Zahtjev nije valjan</response>
-        /// <response> code="503">Baza na koju se spajam nije dostupna</response>
-
-
-        [HttpDelete]
-        [Route("{id:int}")]
-        [Produces("application/json")]
-
-        public IActionResult Delete(int id)
-        {
-
-            if (!ModelState.IsValid || id <= 0) { return BadRequest(); }
-
-            try
-            {
-                var memberFromDB = _context.members.Find(id);
-
-
-                if (memberFromDB == null) { return StatusCode(StatusCodes.Status204NoContent, id); }
-
-                _context.members.Remove(memberFromDB);
-                _context.SaveChanges();
-
-                return new JsonResult(new { message = "Deleted" });
-
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status503ServiceUnavailable, ex.Message);
-            }
-
-
-        }
-
-
     }
 }
+s
