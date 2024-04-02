e8948d51ba4c39409ac5c75dddc911f92a6f3f57
Matija Pavković <46812296+Matija1988@users.noreply.github.com>
2024-04-02 18:04:10 +0200

    Nova ekstenzija

---
+++ PO/PO/Extensions/POExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace PO.Extensions
+{
+    public static class POExtensions
+    {
+        public static void AddPOSwaggerGen(this IServiceCollection Services)
+        {
+            Services.AddSwaggerGen(sgo =>
+            {
+                var o = new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "Project organiser",
+                    Version = "v1",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                    {
+                        Email = "matijapavkovic74@gmail.com",
+                        Name = "Matija Pavkovic"
+                    },
+                    Description = "This is documentation for Project organiser API",
+                    License = new Microsoft.OpenApi.Models.OpenApiLicense()
+                    {
+                        Name = "Education lisence"
+                    }
+                };
+            });
+        }
+    }
+}
+
